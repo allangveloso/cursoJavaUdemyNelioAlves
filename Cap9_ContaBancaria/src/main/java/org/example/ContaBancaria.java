6f6e17c9fbbc2c4ccae54a7e94d641c9550416e3
Allan <allangveloso@gmail.com>
2024-01-24 14:16:02 -0300

    Java_ContaBancaria_V1.1_24012024

--- Cap9_ContaBancaria/src/main/java/org/example/ContaBancaria.java
+++ Cap9_ContaBancaria/src/main/java/org/example/ContaBancaria.java
@@ -3,6 +3,74 @@ package org.example;
 import java.util.Scanner;
 
 public class ContaBancaria {
+<<<<<<< HEAD
+    static String nomeTitular;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ContaCorrente cc = new ContaCorrente();
+
+        int op=0;
+
+        while (op!= 4) {
+            System.out.println("--------------------Menu de opções--------------------");
+            System.out.println("0-Cadastrar conta corrente");
+            System.out.println("1-Alterar o nome do titular");
+            System.out.println("2-Realizar Depósito");
+            System.out.println("3-Realizar Saque");
+            System.out.println("4-Sair");
+            System.out.println("------------------------------------------------------");
+            op = sc.nextInt();
+
+            switch (op) {
+                case 0:
+                    System.out.print("Informe um nome para o titular: ");
+                    nomeTitular = sc.next();
+
+                    System.out.print("Informe o número da conta: ");
+                    cc.numConta = sc.nextInt();
+
+                    System.out.print("Informe um valor para depósito inicial (opcional): ");
+                    cc.saldo = sc.nextDouble();
+                    System.out.println();
+                    exibirDadosContaCorrente(nomeTitular, cc.numConta, cc.saldo);
+
+                    break;
+                case 1:
+                    System.out.print("Informe um novo nome para o titular: ");
+                    nomeTitular = sc.next();
+                    exibirDadosContaCorrente(nomeTitular, cc.numConta, cc.saldo);
+                    break;
+                case 2:
+                    System.out.print("Informe um valor para o depósito: ");
+                    cc.depositar(sc.nextDouble());
+                    exibirDadosContaCorrente(nomeTitular, cc.numConta, cc.saldo);
+                    break;
+                case 3:
+                    System.out.print("Informe valor a sacar: ");
+                    cc.sacar(sc.nextDouble());
+                    exibirDadosContaCorrente(nomeTitular, cc.numConta, cc.saldo);
+                    break;
+                case 4:
+                    System.out.print("Finalizando o programa!");
+                    break;
+                default:
+                    System.out.print("Valor inválido.");
+                    throw new IllegalStateException("Unexpected value: " + sc.nextInt());
+            }
+        }
+    }
+
+    public static void exibirDadosContaCorrente(String nomeTitular, int numConta, double saldo){
+        System.out.println("=================================================================");
+        System.out.printf("Dados da conta corrente: %nConta: " +
+                    "%d%nNome do Titular: " +
+                    "%s%nSaldo: %.2f%n",
+        numConta, nomeTitular, saldo);
+        System.out.println("=================================================================");
+        System.out.println();
+=======
 
     static String nomeTitular;
     public static void main(String[] args) {
@@ -47,5 +115,6 @@ public class ContaBancaria {
                     "%s%nSaldo: %.2f%n",
         numConta, nomeTitular, saldo);
         System.out.println("--------------------------------------------------------");
+>>>>>>> fcf2863d010935a2f2c44c1cd03b056f8ae997a8
     }
 }
--- Cap9_ContaBancaria/src/main/java/org/example/ContaCorrente.java
+++ Cap9_ContaBancaria/src/main/java/org/example/ContaCorrente.java
@@ -6,6 +6,16 @@ public class ContaCorrente {
     //    public String nomeTitular;
     public final double TAXA = 5;
 
+<<<<<<< HEAD
+    public void depositar(double valor) {
+        this.saldo += valor;
+//        return saldo;
+    }
+
+    public void  sacar(double valor) {
+        this.saldo -= (valor + TAXA);
+//        return saldo;
+=======
     public double depositar(double valor) {
         this.saldo += valor;
         return saldo;
@@ -14,5 +24,6 @@ public class ContaCorrente {
     public double sacar(double valor) {
         this.saldo -= (valor + TAXA);
         return saldo;
+>>>>>>> fcf2863d010935a2f2c44c1cd03b056f8ae997a8
     }
 }
