d853bc12ba6e5f37bca4016ffa7970853c619545
Allan <allangveloso@gmail.com>
2024-12-30 09:07:21 -0300

    inclusao Cap8.67_ControleEstoque vazio na main

--- Cap8.67_ControleEstoque/src/main/java/application/App.java
+++ Cap8.67_ControleEstoque/src/main/java/application/App.java
@@ -1,4 +1,23 @@
+/**Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). Em seguida:
+        -Mostrar os dados do produto (nome, preço quantidade no estoque, valor total no estoque)
+        -Realizar uma entrada no estoque e mostrar novamente os dados do produto
+        -Realizar uma saída no estoque e mostrar novamente os dados do produto
+
+        Classe de exemplo
+
+        |Product
+        -------------------
+        -Name: string
+        -Price: double
+        -Quantity: int
+        -------------------
+        +TotalValueInStock(): double
+        +AddProducts(quantity: int): void
+        +RemoveProducts(quantity: int): void
+*/
 package application;
+
+
 
 /**
  * Hello world!
